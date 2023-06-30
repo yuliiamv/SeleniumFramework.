b3477362c9ce9b48d46405b9e20ed10aabd8f33a
yuliiamv <105075739+yuliiamv@users.noreply.github.com>
2023-06-30 14:32:09 -0400

    javahomework

--- src/test/java/testcases/TestCase.java
+++ src/test/java/testcases/TestCase.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class TestCase {
-   /* @Test
+    @Test
     public void startBrowser() {
         System.setProperty("webdriver.chrome.driver", "/Users/juliamyroniv/IdeaProjects/SeleniumProject./src/test/resources/chromedriver");
         ChromeOptions options = new ChromeOptions();
@@ -15,7 +15,7 @@ public class TestCase {
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         ChromeDriver driver = new ChromeDriver(options);
         driver.get("https://www.nytimes.com/");
-    }*/
+    }
 
 
 
