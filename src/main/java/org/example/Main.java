7a8d784c8d2f74e616092ca5d4059c1a1cab3367
yuliiamv <105075739+yuliiamv@users.noreply.github.com>
2023-07-16 20:32:05 -0400

    JavaConditionsAndIfStatements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+<<<<<<< Updated upstream
+=======
+        System.out.println(4*10+"10");
+>>>>>>> Stashed changes
     }
 }
+
+
